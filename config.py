052487e2b7aeae9a63d125447fa8d284c771456b
fpereverzev <158035486+fpereverzev@users.noreply.github.com>
2024-03-19 13:36:37 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = '7037904216:AAGN8xYaj-qHqcxDlc95gWkIwQhe6Gr0ALA'
+keys = {
+    'Эфириум': 'ETH',
+    'Биткоин': 'BTC',
+    'Доллар': 'USD',
+    'Евро': 'EUR',
+    'Солана': 'SOL',
+    'Догикоин': 'DOGE',
+    'Аваланч': 'AVAX',
+    'Фунт': 'GBP'
+}
